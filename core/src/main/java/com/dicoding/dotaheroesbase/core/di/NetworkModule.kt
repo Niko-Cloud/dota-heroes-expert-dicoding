fb0f487769aa4f81c3434440293bd5ae4def6b1f
Niko-Cloud <indraniko20@gmail.com>
2024-01-03 11:55:20 +0700

    Add SSL pinning

--- core/src/main/java/com/dicoding/dotaheroesbase/core/di/NetworkModule.kt
+++ core/src/main/java/com/dicoding/dotaheroesbase/core/di/NetworkModule.kt
@@ -5,6 +5,7 @@ import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
+import okhttp3.CertificatePinner
 import okhttp3.OkHttpClient
 import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
@@ -20,10 +21,15 @@ class NetworkModule {
 
     @Provides
     fun provideOkHttpClient(): OkHttpClient {
+        val certificatePinner = CertificatePinner.Builder()
+            .add("api.opendota.com", "sha256/xCNjlkI9mPUm9o3T8Kz6NpMGFT7d1MITiYjbiNZngM=")
+            .add("api.opendota.com", "sha256/81Wf12bcLlFHQAfJluxnzZ6Frg+oJ9PWY/Wrwur8viQ=")
+            .add("api.opendota.com","sha256/hxqRlPTu1bMS/0DITB1SSu0vd4u/8l8TjPgfaAp63Gc=")
         return OkHttpClient.Builder()
             .addInterceptor(HttpLoggingInterceptor().setLevel(HttpLoggingInterceptor.Level.BODY))
             .connectTimeout(TIME, TimeUnit.SECONDS)
             .readTimeout(TIME, TimeUnit.SECONDS)
+            .certificatePinner(certificatePinner.build())
             .build()
     }
 
